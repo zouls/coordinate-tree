7db8d959b713bdef7351ed8dd010e533f2df4104
zouls <zoulshell@gmail.com>
2019-11-12 09:07:01 +0800

    add iterator pattern

---
+++ coordinate-design-pattern/src/main/java/com/zouls/coordinate_design_pattern/behavioral/iterator/Course.java
@@ -0,0 +1,10 @@
+package com.zouls.coordinate_design_pattern.behavioral.iterator;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Course {
+    private String name;
+}
---
+++ coordinate-design-pattern/src/main/java/com/zouls/coordinate_design_pattern/behavioral/iterator/CourseIteratorImpl.java
@@ -0,0 +1,39 @@
+package com.zouls.coordinate_design_pattern.behavioral.iterator;
+
+import java.util.List;
+
+/**
+ *
+ */
+public class CourseIteratorImpl implements CourseIterator {
+
+    /**
+     * 要迭代的资源集合
+     */
+    private List<Course> courseList;
+    /**
+     * 当前迭代到哪个位置了
+     */
+    private int position;
+    /**
+     * 当前迭代出来的资源
+     */
+    private Course course;
+
+    CourseIteratorImpl(List<Course> courseList) {
+        this.courseList = courseList;
+    }
+
+    @Override
+    public Course nextCourse() {
+        System.out.println("返回课程,位置是: " + position);
+        course = courseList.get(position);
+        position++;
+        return course;
+    }
+
+    @Override
+    public boolean isLastCourse() {
+        return position >= courseList.size();
+    }
+}
